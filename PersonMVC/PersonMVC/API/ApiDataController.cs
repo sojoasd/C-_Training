00ee7783f04dff6a32056b2543bc53af52a9d0d6
zeal <sojoasd@gmail.com>
2015-09-22 17:31:31 +0800

    complete RESTful

--- PersonMVC/PersonMVC/API/ApiDataController.cs
+++ PersonMVC/PersonMVC/API/ApiDataController.cs
@@ -2,6 +2,7 @@
 using PersonMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
@@ -19,7 +20,6 @@ namespace PersonMVC.API
         /// <summary>
         /// 最原始的 GET 回傳，沒有傳入任何引數
         /// </summary>
-        /// <returns></returns>
         public IQueryable GetDemoPerson()
         {
             return db.DemoPersons;
@@ -40,11 +40,10 @@ namespace PersonMVC.API
         /// <summary>
         /// 這裡的 postman
         /// 1. form post : 要選擇 x-www-form-urlencoded
-        /// 2. json post : 要選擇 raw -> json
+        /// 2. json post : 要選擇 raw 再選 json，並將 Content-Type : application/json
         /// ps. 這兩者都不能放入 PersonID 欄位，否則會 binding 沒過
+        /// person = collection data
         /// </summary>
-        /// <param name="person"></param>
-        /// <returns></returns>
         [ResponseType(typeof(DemoPerson))]
         public IHttpActionResult PostCustomer(DemoPerson person)
         {
@@ -71,6 +70,52 @@ namespace PersonMVC.API
             }
 
             return CreatedAtRoute("DefaultApi", new { id = DPDdata.PersonID }, DPDdata);
+        }
+
+        /// <summary>
+        /// post man 作法同上面
+        /// </summary>
+        [ResponseType(typeof(DemoPerson))]
+        public IHttpActionResult PutCustomer(Guid? id, DemoPerson person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != person.PersonID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            //return StatusCode(HttpStatusCode.NoContent);
+            return Ok(person);
+        }
+
+        [ResponseType(typeof(DemoPerson))]
+        public IHttpActionResult DeleteProduct(Guid? id)
+        {
+            DemoPerson person = db.DemoPersons.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            db.DemoPersons.Remove(person);
+            db.SaveChanges();
+
+            return Ok(person);
         }
     }
 }
